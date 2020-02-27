6b0aabb1fb59093c22acdae4e0bde8515c1b2bf6
Triplicity6 <60951154+Triplicity6@users.noreply.github.com>
2020-02-27 03:20:38 -0800

    Update line.js

--- code/line.js
+++ code/line.js
@@ -1,4 +1,4 @@
- margin = {top: 50, right: 100, bottom: 50, left: 600},
+ margin = {top: 50, right: 100, bottom: 50, left: 60},
     width = 1000 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
 
